6805c20070c66bd3141cd0d6b62d950e6a5893a0
Przemek <przemek.popielarski@gmail.com>
2019-03-21 09:50:29 +0100

    InlineKeyboard code modified and tested

--- BotAPI/InlineKeyboard.py
+++ BotAPI/InlineKeyboard.py
@@ -6,26 +6,23 @@ class InlineKeyboard:
         self.max_row = 0
         self.keyboard_string = None
 
-    def __add_to_dictionary(self, button_text, row, column):
+    def add_button(self, text, callback_data, row=None, column=None, url=None):
+        if row is None:
+            row = max(self.row_dict)+1
+        if column is None:
+            column = 1
+        button_text = '{"text":"' + text + '","callback_data":"' + callback_data + \
+                      (',"url":"' + url + '"' if url else '') + '"}'
         if row in self.row_dict:
             self.row_dict[row][column] = button_text
         else:
             self.row_dict[row] = {}
             self.row_dict[row][column] = button_text
 
-    def add_button(self, text, callback_data, row=None, column=None, url=None):
-        if row is None:
-            self.max_row = self.max_row + 1
-            row = self.max_row
-        elif self.max_row < row:
-            self.max_row = row
-        if column is None:
-            column = 1
-        button_text = '{"text":"' + text + '","callback_data":"' + callback_data + (',"url":"' + url + '"' if url else '') + '"}'
-        self.__add_to_dictionary(button_text, row, column)
-
     def delete_button(self, row, column):
-        self.button_dict.pop((row, column), None)
+        self.row_dict[row].pop(column, None)
+        if len(self.row_dict[row]) == 0:
+            self.row_dict.pop(row, None)
 
     def refresh_keyboard_string(self):
         s = "["
@@ -36,3 +33,5 @@ class InlineKeyboard:
             s = s + row_text[:-1]+"],"
         self.keyboard_string = s[:-1] + "]"
 
+    def get_keyboard_string(self):
+        return self.keyboard_string
