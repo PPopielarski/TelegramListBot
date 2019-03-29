6ebc2218e5e98f228a28cf8f8c91802594de3eb0
Przemek <przemek.popielarski@gmail.com>
2019-03-29 10:47:28 +0100

    Moving Chat handler to main thread.

--- ListBot/MainThread.py
+++ ListBot/MainThread.py
@@ -13,33 +13,34 @@ bot.get_updates(timeout=0)
 while True:
 
     # get updates from bot and loop for all of them
-    for result in bot.get_updates():
-
-        if 'callback_query' in result:
-            chat_id = result['callback_query']['message']['chat']['id']
-        elif 'message' in result:
-            chat_id = result['message']['chat']['id']
+    for update in bot.get_updates():
+        # retrieving chat_id
+        if 'callback_query' in update:
+            chat_id = update['callback_query']['message']['chat']['id']
+        elif 'message' in update:
+            chat_id = update['message']['chat']['id']
         else:
             continue
 
+        # creating or reading Chat instance
         if chat_id in chat_dict:
-            chat = chat_dict[chat_id].handle_update(result)
+            chat = chat_dict[chat_id]
         else:
             message = bot.send_message("List Bot", chat_id)['result']
             chat = Chat(chat_id, message['message_id'], message['date'])
             chat_dict[chat_id] = chat
 
-
-#       TODO - DOPASOWAĆ PONIŻSZĄ FUNKCJĘ DO LOOP POWŻEJ. LOOP POWINIEN JĄ WYWOŁYWAĆ DLA KAŻDEGO result.
-    def handle_update(update):
+        # selecting appropriate handler
         if "message" in update:
-            last_message_id = None
+            chat_dict[chat_id].last_message_id = None
             if 'entities' in update['message']:
                 if update['message']['entities']['type'] is "bot_command":
-                    handle_command(update['message'])
+                    pass  # placeholder for handling commands
             else:
-                handle_message(update['message'])
+                pass # placeholder for handling messages
         elif 'callback_query' in update:
-            handle_callback(update['callback_query'])
+            pass # placeholder for handling callback
 
+    # end of handling updates
 
+    # TODO placeholder for handling scheduled tasks
