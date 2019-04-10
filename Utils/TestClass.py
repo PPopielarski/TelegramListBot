55d8b1b538994f417d9fba14554a984449bc4c37
ppopielarski <popielarski.przemyslaw@gmail.com>
2019-04-10 15:24:50 +0200

    Unit tests

--- Utils/TestClass.py
+++ Utils/TestClass.py
@@ -1,20 +1,2 @@
 from BotFramework import ReplyMarkups, Buttons, TelegramBotAPI
 import time
-
-bot_token = '783375470:AAHjxORsSMQRcL3T2RIQgFkQs6ZSt9vpemI'
-bot = TelegramBotAPI.TelegramBotAPI(bot_token)
-
-ik = ReplyMarkups.InlineKeyboardMarkup()
-ik.add_button(Buttons.InlineKeyboardButton('przycisk', callback_data='cbd'), row=1, col=1)
-
-bot.send_message(chat_id=-1001307448947, text='To jest przykładowa klawiatura bota.', reply_markup=ik)
-
-time.sleep(4)
-
-updates = bot.get_updates()
-
-for result in updates['result']:
-    print(result)
-    cal_id = result['callback_query']['id']
-
-bot.answer_callback_query(callback_query_id=cal_id, text='False', show_alert=False, cache_time=10)
---
+++ Utils/UpdateStructure.py
@@ -0,0 +1,71 @@
+
+
+class Entity:
+
+    __slots__ = 'offset', 'length', 'type'
+
+    def __init__(self, entity: dict):
+        self.offset = entity.get('offset', None)
+        self.length = entity.get('length', None)
+        self.type = entity.get('type', None)
+
+
+class Chat:
+
+    __slots__ = 'id', 'first_name', 'last_name', 'username', 'type', 'title'
+
+    def __init__(self, chat: dict):
+        self.id = chat.get('id', None)
+        self.first_name = chat.get('first_name', None)
+        self.last_name = chat.get('last_name', None)
+        self.username = chat.get('username', None)
+        self.type = chat.get('type', None)
+        self.title = chat.get('title', None)
+
+
+class FromUser:
+
+    __slots__ = 'id', 'is_bot', 'first_name', 'last_name', 'username', 'language_code'
+
+    def __init__(self, from_user: dict):
+        self.id = from_user.get('id', None)
+        self.is_bot = from_user.get('is_bot', None)
+        self.first_name = from_user.get('first_name', None)
+        self.last_name = from_user.get('last_name', None)
+        self.username = from_user.get('username', None)
+        self.language_code = from_user.get('language_code', None)
+
+
+class Message:
+
+    __slots__ = 'message_id', 'from_user', 'chat', 'date', 'text', 'entities', 'forward_from_message_id', \
+                'forward_date', 'media_group_id'
+
+    def __init__(self, message: dict):
+        self.message_id = message.get('message_id', None)
+        self.from_user = FromUser(message.get('from', None))
+        self.chat = Chat(message.get('chat', None))
+        self.date = message.get('date', None)
+        self.text = message.get('text', None)
+        self.entities = [message.get['entities']]
+        self.forward_from_message_id = message.get('forward_from_message_id', None)
+        self.forward_date = message.get('forward_date', None)
+        self.media_group_id = message.get('media_group_id', None)
+
+
+class Update:
+
+    __slots__ = 'self.update'
+
+    def __init__(self, update: dict):
+        self.update = update
+
+    def update_id(self) -> int:
+        return self.update['id']
+
+    def message(self) -> Message:
+        return Message(self.update['message'])
+
+
+
+
